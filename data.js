5fb630da8751fb7a423b2a0348b511ed848c10c4
Isahi.Rea <Isahi.rea@live.com>
2025-02-12 22:05:44 -0600

    Started on Add-Btn

--- data.js
+++ data.js
@@ -28,11 +28,16 @@ content.innerHTML = `
     <section class="choice-container">
         ${menuArray.map(item => `
             <div class="choice">
-                <p class="emoji">${item.emoji}</p>
-                <div class="details-container">
-                    <h2 class="product-name">${item.name}</h2>
-                    <p class="ingredients">${item.ingredients.join(", ")}</p>
-                    <p class="price">$${item.price}</p>
+                <div class="product-container">
+                    <p class="emoji">${item.emoji}</p>
+                    <div class="details-container">
+                        <h2 class="product-name">${item.name}</h2>
+                        <p class="ingredients">${item.ingredients.join(", ")}</p>
+                        <p class="price">$${item.price}</p>
+                    </div>
+                </div>
+                <div class="button-container">
+                    <p class="add-btn">+</p>
                 </div>
             </div>
         `).join("")}
